fa467519103c86a9f8069d5aa537fc30f14d89b6
Arraya <markillerth0000@gmail.com>
2026-01-06 23:01:18 +0700

    Done

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+
+int gcd(int a, int b);
 
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+
+int gcd(int a, int b){
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
 }
